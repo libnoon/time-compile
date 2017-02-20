3c8d503437524f9906082d982b411d4966ba0cdb
Fabrice Bauzac <libnoon@gmail.com>
2017-02-20 08:51:12 +0100

    IntervalTest: add a @RunWith annotation

--- src/test/java/fr/fbauzac/IntervalTest.java
+++ src/test/java/fr/fbauzac/IntervalTest.java
@@ -6,7 +6,10 @@ import static org.junit.Assert.assertThat;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class IntervalTest {
 
     @Test
