9b2db8f4a2d0113941d8545b746c1c8231258d75
Fabrice Bauzac <libnoon@gmail.com>
2017-04-08 22:22:14 +0200

    One day is now 7 hours long by default

--- lib/fr/fbauzac/timecompile/Duration.java
+++ lib/fr/fbauzac/timecompile/Duration.java
@@ -4,7 +4,7 @@ public final class Duration {
 
     private final int minutes;
 
-    private Duration(int minutes) {
+    Duration(int minutes) {
 	this.minutes = minutes;
     }
 
@@ -12,13 +12,22 @@ public final class Duration {
 	return new Duration(minutes);
     }
 
+    private static int getDayLength() {
+	String str = System.getProperty("fr.fbauzac.time-compile.day-length");
+	if (str != null) {
+	    return Integer.parseInt(str);
+	} else {
+	    return 7;
+	}
+    }
+
     @Override
     public String toString() {
 	int accu = minutes;
 	int min = accu % 60;
 	accu /= 60;
-	int hour = accu % 24;
-	accu /= 24;
+	int hour = accu % getDayLength();
+	accu /= getDayLength();
 	int days = accu;
 
 	StringBuilder sb = new StringBuilder();
